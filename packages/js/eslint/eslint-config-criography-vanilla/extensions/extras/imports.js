f8df3f3a66e074b89c143253fb19cfb247315a54
marek <reg@criography.com>
2020-03-10 21:10:09 +0000

    fixing dependencies

--- packages/js/eslint/eslint-config-criography-vanilla/extensions/extras/imports.js
+++ packages/js/eslint/eslint-config-criography-vanilla/extensions/extras/imports.js
@@ -253,7 +253,7 @@ module.exports = {
 
         // Require modules with a single export to use a default export
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
-        'import/prefer-default-export' : 'error',
+        'import/prefer-default-export' : 'warn',
 
 
         // Forbid modules to have too many dependencies
